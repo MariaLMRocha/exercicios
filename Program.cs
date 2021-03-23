f15d68550a0c861c6c880e6c23f54744c156493c
Maria Luiza Moura Rocha <79229879+MariaLMRocha@users.noreply.github.com>
2021-03-22 21:29:43 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace exercicioSoma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2 = 0;
+            double soma = 0.0;
+
+            Console.Write("Entre com um número: ");
+            num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Entre com outro número: ");
+            num2 = int.Parse(Console.ReadLine());
+
+            soma = num1 + num2;
+
+            Console.Write("SOMA = " + soma);
+        }
+    }
+}
